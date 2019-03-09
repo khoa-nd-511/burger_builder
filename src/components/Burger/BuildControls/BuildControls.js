f3c29f7a7c2e756194798b59604322ebe3596cb7
Khoa Nguyen <37206870+EricKhoaMonger@users.noreply.github.com>
2019-03-10 01:11:11 +0700

    Update BuildControls.js

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,11 +14,11 @@ const buildControls = props => (
   <div className={classes.BuildControls}>
     <div className={classes.OrderAndPrice}>
       <div className={classes.PriceBar}>
-        Total Price: ${props.totalPrice.toFixed(2)}
+        Total Price: {props.totalPrice ? props.totalPrice.toFixed(2) : null }
       </div>
       <button
         className={classes.OrderButton}
-        disabled={!props.showOrderBtn}
+        disabled={props.showOrderBtn}
         onClick={props.ordered}
       >
         Order Now
